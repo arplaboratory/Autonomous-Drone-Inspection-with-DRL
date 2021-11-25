dc04c71a347f8eabbafa7acf48902758a6fd35a6
xjh19971 <jx1190@nyu.edu>
2021-11-24 22:28:56 -0500

    update env

--- training.py
+++ training.py
@@ -1,3 +1,19 @@
+import argparse
+
 import gym
-from stable_baselines3 import SAC
+
+from stable_baselines3.sac import SAC
+from stable_baselines3.common.vec_env import SubprocVecEnv
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    # data params
+    parser.add_argument('-policy', type=str, default='cnn', choices='cnn')
+
+    opt = parser.parse_args()
+
+    if opt.policy == 'cnn':
+        policy_name = 'CnnPolicy'
+
+    model = SAC()
 
